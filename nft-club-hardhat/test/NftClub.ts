1835906f7e40382624f691554f9f1b60016b08e1
Asem-Abdelhady <asemshawkey@gmail.com>
2023-11-22 16:42:13 +0300

    Add: test cases for NFT

--- nft-club-hardhat/test/NftClub.ts
+++ nft-club-hardhat/test/NftClub.ts
@@ -1,26 +1,23 @@
-import { ethers, deployments, getNamedAccounts, network } from "hardhat";
-import { expect } from "chai";
-import { NftClub, ERC721Generator } from "../typechain-types";
-import { readFileSync, writeFileSync } from "fs";
-import { frontEndContractsFile } from "../helper-hardhat-config";
-import ChainData from "../types/ChainData";
-import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
+import { ethers, deployments, getNamedAccounts, network } from 'hardhat';
+import { expect } from 'chai';
+import { NftClub } from '../typechain-types';
+import { readFileSync } from 'fs';
+import { frontEndContractsFile } from '../helper-hardhat-config';
+import ChainData from '../types/ChainData';
+import { HardhatEthersSigner } from '@nomicfoundation/hardhat-ethers/signers';
 
-describe("NftClub and ERC721Generator Contracts", function () {
+describe('NftClub and ERC721Generator Contracts', function () {
   let nftClub: NftClub;
-  let deployer: string;
   let otherAccount: string;
   let accounts: HardhatEthersSigner[];
   beforeEach(async function () {
-
-    const contractAddress = readFileSync(frontEndContractsFile, "utf-8");
-  
+    const contractAddress = readFileSync(frontEndContractsFile, 'utf-8');
     let chainData: ChainData = {};
     if (contractAddress) {
       try {
         chainData = JSON.parse(contractAddress);
       } catch (error) {
-        console.error("Error parsing chain data JSON:", error);
+        console.error('Error parsing chain data JSON:', error);
         return;
       }
     }
@@ -33,23 +30,24 @@ describe("NftClub and ERC721Generator Contracts", function () {
     }
 
     const nftClubAddress = chainData[chainId];
-  
-    accounts = await ethers.getSigners();
-    deployer = accounts[0].address;
-    otherAccount = accounts[1].address;
 
-    await deployments.fixture(["NftClub", "ERC721Generator"]);
-    const nftClubContract = await ethers.getContractAt("NftClub", nftClubAddress);
+    accounts = await ethers.getSigners();
+
+    await deployments.fixture(['NftClub', 'all']);
+    const nftClubContract = await ethers.getContractAt(
+      'NftClub',
+      nftClubAddress
+    );
     nftClub = nftClubContract.connect(accounts[0]);
   });
 
-  describe("NftClub - Create Collection", function () {
-    it("Should allow the owner to create a collection", async function () {
-      const collectionName = "Test Collection";
-      const collectionSymbol = "TEST";
-      const collectionPrice = ethers.utils.parseEther("0.1");
-      const collectionUri = "test_uri";
-  
+  describe('NftClub - Create Collection', function () {
+    it('Should allow the owner to create a collection', async function () {
+      const collectionName = 'Test Collection';
+      const collectionSymbol = 'TEST';
+      const collectionPrice = ethers.parseEther('0.1');
+      const collectionUri = 'test_uri';
+
       await expect(
         nftClub.createCollection(
           collectionName,
@@ -58,29 +56,130 @@ describe("NftClub and ERC721Generator Contracts", function () {
           collectionUri
         )
       ).to.not.be.reverted;
-  
+
       const collection = await nftClub.s_collections(0);
+
       expect(collection.name).to.equal(collectionName);
       expect(collection.URI).to.equal(collectionUri);
     });
-  
-    it("Should fail if a non-owner tries to create a collection", async function () {
-      const collectionName = "Test Collection";
-      const collectionSymbol = "TEST";
-      const collectionPrice = ethers.utils.parseEther("0.1");
-      const collectionUri = "test_uri";
-  
-      await expect(
-        nftClub.connect(accounts[1]).createCollection(
-          collectionName,
-          collectionSymbol,
-          collectionPrice,
-          collectionUri
-        )
-      ).to.be.revertedWith("NftClub__NotOwner");
-    });
-  
-  });
-  
 
+    it('Should fail if a non-owner tries to create a collection', async function () {
+      const collectionName = 'Test Collection';
+      const collectionSymbol = 'TEST';
+      const collectionPrice = ethers.parseEther('0.1');
+      const collectionUri = 'test_uri';
+
+      try {
+        await nftClub
+          .connect(accounts[1])
+          .createCollection(
+            collectionName,
+            collectionSymbol,
+            collectionPrice,
+            collectionUri
+          );
+      } catch (error: any) {
+        expect(error.message).to.include('NftClub__NotOwner()');
+        return;
+      }
+    });
+  });
+
+  describe('NftClub - Buy NFT', function () {
+    it('Should allow a user to buy an NFT from a collection', async function () {
+      const collectionName = 'Test Collection';
+      const collectionSymbol = 'TEST';
+      const collectionPrice = ethers.parseEther('0.1');
+      const collectionUri = 'test_uri';
+      await nftClub.createCollection(
+        collectionName,
+        collectionSymbol,
+        collectionPrice,
+        collectionUri
+      );
+
+      const initialOwner = await nftClub.verifyOwnership(
+        0,
+        accounts[0].address
+      );
+      expect(initialOwner).to.be.false;
+
+      await nftClub.buyNft(0, { value: collectionPrice });
+
+      const newOwner = await nftClub.verifyOwnership(0, accounts[0].address);
+      expect(newOwner).to.be.true;
+    });
+
+    it('Should fail to buy an NFT with insufficient payment', async function () {
+      const collectionName = 'Test Collection';
+      const collectionSymbol = 'TEST';
+      const collectionPrice = ethers.parseEther('0.1');
+      const collectionUri = 'test_uri';
+      await nftClub.createCollection(
+        collectionName,
+        collectionSymbol,
+        collectionPrice,
+        collectionUri
+      );
+
+      try {
+        await nftClub.buyNft(0, { value: ethers.parseEther('0.05') });
+      } catch (error: any) {
+        expect(error.message).to.include('NftClub__IncorrectPrice()');
+        return;
+      }
+    });
+
+    it('Should fail to buy an NFT from a non-existent collection', async function () {
+      try {
+        await nftClub.buyNft(999);
+      } catch (error: any) {
+        expect(error.message).to.include('NftClub__CollectionDoesNotExist()');
+        return;
+      }
+    });
+  });
+
+  describe('NftClub - Verify Ownership', function () {
+    it('Should return true for the owner of an NFT', async function () {
+      const collectionName = 'Test Collection';
+      const collectionSymbol = 'TEST';
+      const collectionPrice = ethers.parseEther('0.1');
+      const collectionUri = 'test_uri';
+      await nftClub.createCollection(
+        collectionName,
+        collectionSymbol,
+        collectionPrice,
+        collectionUri
+      );
+
+      const owner = await nftClub.verifyOwnership(0, accounts[0].address);
+      expect(owner).to.be.false;
+    });
+
+    it('Should return false for a non-owner of an NFT', async function () {
+      const collectionName = 'Test Collection';
+      const collectionSymbol = 'TEST';
+      const collectionPrice = ethers.parseEther('0.1');
+      const collectionUri = 'test_uri';
+      await nftClub.createCollection(
+        collectionName,
+        collectionSymbol,
+        collectionPrice,
+        collectionUri
+      );
+
+      const owner = await nftClub.verifyOwnership(0, accounts[1].address);
+      expect(owner).to.be.false;
+    });
+
+    it('Should fail to verify ownership for a non-existent collection', async function () {
+      try {
+        nftClub.verifyOwnership(999, accounts[0].address);
+      } catch (error: any) {
+        expect(error.message).to.include('NftClub__CollectionDoesNotExist()');
+        return;
+      }
+    });
+  });
 });
